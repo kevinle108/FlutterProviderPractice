b53836a2c966e1f7dcb16161cd48faa25882c3c6
Kevin Le <kevinle108@gmail.com>
2021-06-28 11:35:43 -0400

    fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String passedData = 'My top secret data';
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: MyText(),),
+//           floatingActionButton: FloatingActionButton(
+//             onPressed: () {
+// \            },
+//           ),
+          body: Level1(),
+        ),
       ),
-      home: Level1(data: passedData),
     );
   }
 }
 
 
 class Level1 extends StatelessWidget {
-  late String data;
 
-  Level1({required this.data});
 
   @override
   Widget build(BuildContext context) {
-    return Level2(data: this.data);
+    return Level2();
   }
 }
 
 class Level2 extends StatelessWidget {
-  late String data = 'level2';
 
-  Level2({required this.data});
 
 
   @override
   Widget build(BuildContext context) {
-    return Level3(data: this.data);
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
+    );
   }
 }
 
 class Level3 extends StatelessWidget {
-  late String data;
-
-  Level3({required this.data});
 
 
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return MyText();
   }
 }
 
+class MyText extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newVal) {
+        Provider.of<Data>(context, listen: false).changeString(newVal);
+
+      },
+    );
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'From inside Data class';
+
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
+  }
+}
